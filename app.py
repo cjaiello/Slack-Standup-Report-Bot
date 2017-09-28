f9fce5989dc15694ea574ecdf949027d8b11ef49
Christina <christinajaiello@gmail.com>
2017-09-28 15:52:38 -0400

    Having bot ping channel

--- app.py
+++ app.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 sched = BackgroundScheduler()
 slack_client = SlackClient(os.environ['SLACKID'])
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=15, minute=50)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=15, minute=54)
 def scheduled_job():
     slack_client.api_call(
       "chat.postMessage",
       channel="#christinastestchannel",
-      text="@channel Please reply here with your standup status if you won't be in the office today!",
+      text="<!channel> Please reply here with your standup status if you won't be in the office today!",
       username="Standup Bot"
     )
     print("Standup alert message sent on " + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
