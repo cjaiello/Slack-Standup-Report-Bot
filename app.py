35b46e938abad6af61ca8db2452e70d016cb0001
Christina <christinajaiello@gmail.com>
2017-09-28 15:38:39 -0400

    Removing unneeded code

--- app.py
+++ app.py
@@ -1,25 +1,20 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from flask import Flask, request, Response, jsonify
 from slackclient import SlackClient
 import os
 from time import gmtime, strftime
 
-app = Flask(__name__)
 sched = BackgroundScheduler()
 slack_client = SlackClient(os.environ['SLACKID'])
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=19, minute=32)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=15, minute=39)
 def scheduled_job():
     slack_client.api_call(
       "chat.postMessage",
       channel="#christinastestchannel",
-      text="Please reply here with your standup status if you won't be in the office today!"
+      text="Please reply here with your standup status if you won't be in the office today!",
+      username="Standup Bot"
     )
     print("Standup alert message sent on " + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
-
 sched.start()
-print("test 2")
+print("Standup bot was scheduled on " + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
