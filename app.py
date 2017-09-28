abcaf08e9d65b18e78ec0884e78caeef3025e87e
Christina <christinajaiello@gmail.com>
2017-09-28 13:50:13 -0400

    Initial test to see if this is working

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from flask import Flask, request, Response, jsonify
+from slackclient import SlackClient
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=9)
+slack_token = os.environ["SLACKID"]
+sc = SlackClient(slack_token)
+
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=13, minute=51)
 def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    sc.api_call(
+      "chat.postMessage",
+      channel="#christinastestchannel",
+      text="Please reply here with your standup status if you won't be in the office today!"
+    )
 
 sched.start()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
