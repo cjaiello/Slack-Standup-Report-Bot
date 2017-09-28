660052d72d49848c4a24be6622f9d39abfcc1b7d
Christina <christinajaiello@gmail.com>
2017-09-28 14:00:27 -0400

    Initial test to see if this is working

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ sched = BlockingScheduler()
 slack_token = os.environ["SLACKID"]
 slack_client = SlackClient(slack_token)
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=14)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=14, minute=1)
 def scheduled_job():
     slack_client.api_call(
       "chat.postMessage",
       channel="#christinastestchannel",
       text="Please reply here with your standup status if you won't be in the office today!"
     )
+    print("Standup message sent")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
