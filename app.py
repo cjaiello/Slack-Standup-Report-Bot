5e54b6249a0913c3746582ff8d787a73eac6a9cb
Christina <christinajaiello@gmail.com>
2017-09-28 15:30:22 -0400

    Adding timestamp

--- app.py
+++ app.py
@@ -2,29 +2,20 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, request, Response, jsonify
 from slackclient import SlackClient
 import os
+from time import gmtime, strftime
+
 app = Flask(__name__)
 sched = BackgroundScheduler()
 slack_client = SlackClient(os.environ['SLACKID'])
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=19, minute=27)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=19, minute=31)
 def scheduled_job():
     slack_client.api_call(
       "chat.postMessage",
       channel="#christinastestchannel",
       text="Please reply here with your standup status if you won't be in the office today!"
     )
-    print("Standup message sent")
-
-@app.route('/run')
-def run_bot():
-    sched.start()
-    print("test")
-
-@app.route('/')
-def homepage():
-    return """
-    KarmaBot
-    """
+    print("Standup alert message sent on " + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
