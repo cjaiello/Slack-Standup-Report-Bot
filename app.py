6ee25430ce6b4bdd22b599c842759b5da2bcd5f3
Christina <christinajaiello@gmail.com>
2017-09-28 15:58:42 -0400

    Changing emoji to memo emoji

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ app = Flask(__name__)
 sched = BackgroundScheduler()
 slack_client = SlackClient(os.environ['SLACKID'])
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=15, minute=54)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=16)
 def scheduled_job():
     slack_client.api_call(
       "chat.postMessage",
       channel="#christinastestchannel",
       text="<!channel> Please reply here with your standup status if you won't be in the office today!",
-      username="Standup Bot"
+      username="Standup Bot",
+      icon_emoji=":memo:"
     )
     print("Standup alert message sent on " + strftime("%Y-%m-%d %H:%M:%S", gmtime()))
 
