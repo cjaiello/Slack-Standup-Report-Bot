8e51d2e3f3d5d15d743360cdaa89abd245c0ed9a
Christina <christinajaiello@gmail.com>
2017-09-28 22:02:22 -0400

    Adding wtforms to requirements

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ slack_client = SlackClient(os.environ['SLACK_BOT_TOKEN'])
 
 standup_dictionary = {'christinastestchannel' : 4}
 
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=22 minutes=2)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=22, minutes=3)
 def scheduled_job():
     slack_client.api_call(
       "chat.postMessage",
