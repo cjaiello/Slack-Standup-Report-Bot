168f27e4433eb5c003e9aab4687a4a15ea457fd2
Christina <christinajaiello@gmail.com>
2017-09-29 09:56:07 -0400

    No longer tracking sqlalchemy modifications

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import psycopg2
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 sched = BackgroundScheduler()
 slack_client = SlackClient(os.environ['SLACK_BOT_TOKEN'])
