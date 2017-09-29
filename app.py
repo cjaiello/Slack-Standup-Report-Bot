92540803f5e3ca8f94a8cf91fea03ca49ea184bf
Christina <christinajaiello@gmail.com>
2017-09-29 00:25:40 -0400

    Alternative to lambda

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def set_schedules():
         #sched.add_job(standup_call(channel.channel_name), 'cron', day_of_week='mon-fri', hour=channel.standup_time, id=channel.id)
         print("Channel name that we're setting the schedule for: " + channel.channel_name)
         print("Time: " + str(channel.standup_time))
-        sched.add_job(lambda channel_name=channel.channel_name: standup_call(channel.channel_name), 'cron', day_of_week='mon-fri', hour=0, minute=22, id=channel.channel_name)
+        sched.add_job(standup_call, 'cron', channel.channel_name, day_of_week='mon-fri', hour=0, minute=27, id=channel.channel_name)
 
 
 # Function that triggers the standup call
