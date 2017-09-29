f7a6450735e46dd24703875e86bb20ad0987aa2b
Christina <christinajaiello@gmail.com>
2017-09-28 23:40:22 -0400

    Changing time

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def set_schedules():
         #sched.add_job(standup_call(channel.channel_name), 'cron', day_of_week='mon-fri', hour=channel.standup_time, id=channel.id)
         print("Channel name that we're setting the schedule for: " + channel.channel_name)
         print("Time: " + str(channel.standup_time))
-        sched.add_job(lambda: standup_call(channel.channel_name), 'cron', day_of_week='mon-fri', hour=23, minute=40, id=channel.channel_name)
+        sched.add_job(lambda: standup_call(channel.channel_name), 'cron', day_of_week='mon-fri', hour=23, minute=42, id=channel.channel_name)
 
 
 # Function that triggers the standup call
