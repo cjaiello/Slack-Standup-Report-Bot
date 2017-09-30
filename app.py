b8a95af631b94e27b139d5c912dcca55507bb0ce
Christina <christinajaiello@gmail.com>
2017-09-29 21:07:57 -0400

    Adding functionality to let users set standup message and standup minutes

--- app.py
+++ app.py
@@ -21,11 +21,15 @@ class Channel(db.Model):
     __tablename__ = "channel"
     id = db.Column(db.Integer, primary_key=True)
     channel_name = db.Column(db.String(120), unique=True)
-    standup_time = db.Column(db.Integer)
+    standup_hour = db.Column(db.Integer)
+    standup_minute = db.Column(db.Integer)
+    message = db.Column(db.String(120), unique=False)
 
-    def __init__(self, channel_name, standup_time):
+    def __init__(self, channel_name, standup_hour, standup_minute, message):
         self.channel_name = channel_name
-        self.standup_time = standup_time
+        self.standup_hour = standup_hour
+        self.standup_minute = standup_minute
+        self.message = message
 
     def __repr__(self):
         return '<Channel %r>' % self.channel_name
@@ -34,7 +38,9 @@ class Channel(db.Model):
 # Our form model
 class ReusableForm(Form):
     submitted_channel_name = TextField('Channel Name:', validators=[validators.required()])
-    standup_time = TextField('Standup Time:', validators=[validators.required()])
+    standup_hour = TextField('Standup Hour:', validators=[validators.required()])
+    standup_minute = TextField('Standup Minute:', validators=[validators.required()])
+    message = TextField('Standup Message:', validators=[validators.required()])
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -44,29 +50,32 @@ def homepage():
     if request.method == 'POST':
         # Get whatever name they gave us for a channel
         submitted_channel_name = request.form['submitted_channel_name']
-        standup_time = request.form['standup_time']
+        standup_hour = request.form['standup_hour']
+        standup_minute = request.form['standup_minute']
+        message = request.form['message']
         # If the form field was valid...
         if form.validate():
             # Look for channel in database
             if not db.session.query(Channel).filter(Channel.channel_name == submitted_channel_name).count():
                 # Channel isn't in database. Create our channel object
-                channel = Channel(submitted_channel_name, standup_time)
+                channel = Channel(submitted_channel_name, standup_hour, standup_minute, message)
                 # Add it to the database
                 db.session.add(channel)
                 db.session.commit()
                 # Adding this additional job to the queue
-                sched.add_job(standup_call, 'cron', [channel.channel_name], day_of_week='mon-fri', hour=standup_time, id=channel.channel_name)
-                print(strftime("%Y-%m-%d %H:%M:%S", localtime()) + ": Set " + submitted_channel_name + "'s standup time to " + str(standup_time))
+                sched.add_job(standup_call, 'cron', [channel.channel_name, message], day_of_week='mon-fri', hour=standup_hour, minute=standup_minute, id=channel.channel_name)
+                print(strftime("%Y-%m-%d %H:%M:%S", localtime()) + ": Set " + submitted_channel_name + "'s standup time to " + str(standup_hour))
             else:
                 # If channel is in database, update channel's standup time
                 channel = Channel.query.filter_by(channel_name = submitted_channel_name).first()
-                channel.standup_time = standup_time
+                channel.standup_hour = standup_hour
+                channel.standup_minute = standup_minute
                 db.session.commit()
                 # Updating this job's timing
-                sched.reschedule_job(channel.channel_name, trigger='cron', day_of_week='mon-fri', hour=standup_time)
-                print(strftime("%Y-%m-%d %H:%M:%S", localtime()) + ": Updated " + submitted_channel_name + "'s standup time to " + str(standup_time))
+                sched.reschedule_job(channel.channel_name, trigger='cron', day_of_week='mon-fri', hour=standup_hour, minute=standup_minute)
+                print(strftime("%Y-%m-%d %H:%M:%S", localtime()) + ": Updated " + submitted_channel_name + "'s standup time to " + str(standup_hour))
         else:
-            print(strftime("%Y-%m-%d %H:%M:%S", localtime()) + ": Could not update " + submitted_channel_name + "'s standup time to " + str(standup_time))
+            print(strftime("%Y-%m-%d %H:%M:%S", localtime()) + ": Could not update " + submitted_channel_name + "'s standup time to " + str(standup_hour) + ":" + str(standup_minute) + ". Issue was: " + request)
 
     return render_template('homepage.html', form=form)
 
@@ -78,16 +87,16 @@ def set_schedules():
     # Loop through our results
     for channel in channels_with_scheduled_standups:
         # Add a job for each row in the table
-        sched.add_job(standup_call, 'cron', [channel.channel_name], day_of_week='mon-fri', hour=channel.standup_time, id=channel.channel_name)
-        print(strftime("%Y-%m-%d %H:%M:%S", localtime()) + ": Channel name and time that we set the schedule for: " + channel.channel_name + " at " + str(channel.standup_time))
+        sched.add_job(standup_call, 'cron', [channel.channel_name, channel.message], day_of_week='mon-fri', hour=channel.standup_hour, minute=channel.standup_minute, id=channel.channel_name)
+        print(strftime("%Y-%m-%d %H:%M:%S", localtime()) + ": Channel name and time that we set the schedule for: " + channel.channel_name + " at " + str(channel.standup_hour) + ":" + str(channel.standup_minute))
 
 
 # Function that triggers the standup call
-def standup_call(channel_name):
+def standup_call(channel_name, message):
     result = slack_client.api_call(
       "chat.postMessage",
       channel=str(channel_name),
-      text="<!channel> Please reply here with your standup status if you won't be in the office today!",
+      text= "<!channel> " + ("Please reply here with your standup status!" if (message == null) else  message),
       username="Standup Bot",
       icon_emoji=":memo:"
     )
