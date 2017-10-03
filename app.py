360b54ff995b20347bd66bfbe12c28e5de8ec411
Christina <christinajaiello@gmail.com>
2017-10-03 17:40:22 -0400

    Changing environment key

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import psycopg2
 from flask_sqlalchemy import SQLAlchemy
 import yagmail
 
-yagmail.register(os.environ['USERNAME'], os.environ['PW'])
+yagmail.register(os.environ['USERNAME'], os.environ['PASSWORD'])
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
