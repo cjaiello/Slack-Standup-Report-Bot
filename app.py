41b9ff0cca62cd45c0887dd0fc78e7acddae52c8
Christina <christinajaiello@gmail.com>
2017-10-11 19:28:31 -0400

    Extracting text from standup messages and putting into list

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def set_email_job(channel):
         # Add a job for each row in the table, sending standup replies to chosen email.
         # Sending this at 1pm every day
         # TODO: Change back to 1pm, not some other random hour and minutes
-        SCHEDULER.add_job(get_timestamp_and_send_email, 'cron', [channel.channel_name, channel.email], day_of_week='mon-fri', hour=23, minute=53, id=channel.channel_name + "_sendemail")
+        SCHEDULER.add_job(get_timestamp_and_send_email, 'cron', [channel.channel_name, channel.email], day_of_week='mon-fri', hour=19, minute=30, id=channel.channel_name + "_sendemail")
         print(create_logging_label() + "Channel that we set email schedule for: " + channel.channel_name)
     else:
         print(create_logging_label() + "Channel " + channel.channel_name + " did not want their standups emailed to them today.")
@@ -215,14 +215,27 @@ def get_standup_replies_for_message(timestamp, channel_name):
       "channels.history",
       token=os.environ['SLACK_BOT_TOKEN'],
       channel=channel_id,
-      latest=timestamp,
+      latest="1507693919.000178", # TODO: change back to timestamp
       inclusive=True,
       count=1
     )
     if ("ok" in result):
         # TODO: Get the replies from this thread
         print(create_logging_label() + "User standup messages: " + str(result))
-        return str(result.get("messages")).replace("[","").replace("]","")
+        standup_results = []
+        for standup_status in result.get("messages").get("replies"):
+            result = SLACK_CLIENT.api_call(
+              "channels.history",
+              token=os.environ['SLACK_BOT_TOKEN'],
+              channel=channel_id,
+              latest=standup_status.get("ts"),
+              inclusive=True,
+              count=1
+            )
+            standup_results.append(result.get("messages").get("text"))
+            print(esult.get("messages").get("text"))
+        print(standup_results)
+        return standup_results
     else:
         # Log that it didn't work
         print(create_logging_label() + "Tried to retrieve standup results. Could not retrieve standup results for " + channel_name + " due to: " + str(result.error))
