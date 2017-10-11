ea6d175647df7af2054b9d7dbd6b6cbfe3403ff3
Christina <christinajaiello@gmail.com>
2017-10-11 19:34:35 -0400

    Extracting text from standup messages and putting into list

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def set_email_job(channel):
         # Add a job for each row in the table, sending standup replies to chosen email.
         # Sending this at 1pm every day
         # TODO: Change back to 1pm, not some other random hour and minutes
-        SCHEDULER.add_job(get_timestamp_and_send_email, 'cron', [channel.channel_name, channel.email], day_of_week='mon-fri', hour=19, minute=32, id=channel.channel_name + "_sendemail")
+        SCHEDULER.add_job(get_timestamp_and_send_email, 'cron', [channel.channel_name, channel.email], day_of_week='mon-fri', hour=19, minute=35, id=channel.channel_name + "_sendemail")
         print(create_logging_label() + "Channel that we set email schedule for: " + channel.channel_name)
     else:
         print(create_logging_label() + "Channel " + channel.channel_name + " did not want their standups emailed to them today.")
@@ -214,7 +214,7 @@ def get_standup_replies_for_message(timestamp, channel_name):
     result = SLACK_CLIENT.api_call(
       "channels.history",
       token=os.environ['SLACK_BOT_TOKEN'],
-      channel=channel_id,
+      channel="C7A16MBB4", # TODO: change back to channel_id
       latest="1507693919.000178", # TODO: change back to timestamp
       inclusive=True,
       count=1
