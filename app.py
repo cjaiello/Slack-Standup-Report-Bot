e5429c6ca26c8137a52e0a6796f32cdef532d1fe
Christina <christinajaiello@gmail.com>
2017-10-10 22:54:54 -0400

    Adding more logging

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ def set_email_job(channel):
         # Add a job for each row in the table, sending standup replies to chosen email.
         # Sending this at 1pm every day
         # TODO: Change back to 1pm, not some other random hour and minutes
-        SCHEDULER.add_job(get_timestamp_and_send_email, 'cron', [channel.channel_name, channel.email], day_of_week='mon-fri', hour=22, minute=53, id=channel.channel_name + "_sendemail")
+        SCHEDULER.add_job(get_timestamp_and_send_email, 'cron', [channel.channel_name, channel.email], day_of_week='mon-fri', hour=22, minute=57, id=channel.channel_name + "_sendemail")
         print(create_logging_label() + "Channel name and time that we set email schedule for: " + channel.channel_name)
     else:
         print(create_logging_label() + "Channel " + channel.channel_name + " did not want their standups emailed to them today.")
@@ -179,7 +179,8 @@ def get_timestamp_and_send_email(a_channel_name, recipient_email_address):
     channel = Channel.query.filter_by(channel_name = a_channel_name).first()
     if (channel.timestamp != None):
         # First we need to get all replies to this message:
-        standups = get_standup_replies_for_message(channel.timestamp, a_channel_name)
+        print("!! Params: " + channel.timestamp + channel.channel_name)
+        standups = get_standup_replies_for_message(channel.timestamp, channel.channel_name)
 
         # Then we need to send an email with this information
         server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -208,7 +209,7 @@ def get_standup_replies_for_message(timestamp, channel_name):
     result = SLACK_CLIENT.api_call(
       "channels.history",
       token=os.environ['SLACK_BOT_TOKEN'],
-      channel=str(channel_name),
+      channel=channel_name,
       latest=timestamp,
       inclusive=True,
       count=1
