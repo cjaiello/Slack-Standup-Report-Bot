8f6f533162e06bfa57632e766e14a6444e4376f5
Christina <christinajaiello@gmail.com>
2017-11-23 20:49:27 -0500

    Removing channel file

--- app.py
+++ app.py
@@ -174,6 +174,29 @@ def send_email(channel_name, recipient_email_address, email_content):
     server.quit()
 
 
+# Create our database model
+class Channel(DB.Model):
+    __tablename__ = "channel"
+    id = DB.Column(DB.Integer, primary_key=True)
+    channel_name = DB.Column(DB.String(120), unique=True)
+    standup_hour = DB.Column(DB.Integer)
+    standup_minute = DB.Column(DB.Integer)
+    message = DB.Column(DB.String(120), unique=False)
+    email = DB.Column(DB.String(120), unique=False)
+    timestamp = DB.Column(DB.String(120), unique=False)
+
+    def __init__(self, channel_name, standup_hour, standup_minute, message, email, timestamp):
+        self.channel_name = channel_name
+        self.standup_hour = standup_hour
+        self.standup_minute = standup_minute
+        self.message = message
+        self.email = email
+        self.timestamp = timestamp
+
+    def __repr__(self):
+        return '<Channel %r>' % self.channel_name
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
 
