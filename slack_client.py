1984709a9e68384e13411b8105d6dd4eee2b13ff
Christina <christinajaiello@gmail.com>
2020-09-16 21:20:30 -0400

    Changing confirmation logging

--- slack_client.py
+++ slack_client.py
@@ -13,6 +13,9 @@ def send_standup_message(channel_name, message):
       username="Standup Bot",
       icon_emoji=":memo:"
   )
+  print(util.create_logging_label() + "send_standup_message response " + str(response.status_code))
+  for item in response.data.items():
+    print(util.create_logging_label() + "send_standup_message response" + str(item[0]) + ": " + str(item[1]))
   return response.status_code
 
 # Will send confirmation message to @param channel_name
@@ -23,9 +26,9 @@ def send_confirmation_message(channel_name, message):
       username="Standup Bot",
       icon_emoji=":memo:"
   )
-  print(util.create_logging_label() + str(response.status_code))
+  print(util.create_logging_label() + "send_confirmation_message response " + str(response.status_code))
   for item in response.data.items():
-    print(util.create_logging_label() + " " + str(item[0]) + " " + str(item[1]))
+    print(util.create_logging_label() + "send_confirmation_message response" + str(item[0]) + ": " + str(item[1]))
   return response.status_code
 
 # Will fetch the standup messages for a channel
