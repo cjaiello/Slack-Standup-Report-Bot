fe45a4694a8a7420e80d06ef8b1722f7711cd3ef
Christina <christinajaiello@gmail.com>
2020-09-18 13:22:52 -0400

    jk that was a different bug

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 # Standup Bot by Christina Aiello, 2017
 import re
+import logger
 
 # For logging purposes
 def format_minutes_to_have_zero(minutes):
@@ -22,8 +23,10 @@ def remove_starting_zeros_from_time(time):
 # data from the form into the database, we convert from AM/PM
 # to 24-hour time.
 def calculate_am_or_pm(reminder_hour, am_or_pm):
+    logger.log("Reminder hour is: " + str(reminder_hour) " and am or pm is: " + am_or_pm, "INFO")
+    reminder_hour = int(reminder_hour)
     if (am_or_pm == "pm" and reminder_hour != 12):
-        reminder_hour = int(reminder_hour) + 12
+        reminder_hour =  + 12
     elif (am_or_pm == "am" and reminder_hour == 12):
         reminder_hour = 0
     return reminder_hour
