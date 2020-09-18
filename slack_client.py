135e57ed0cd9cec6c83238a1f6bd34e1211e166c
Christina <christinajaiello@gmail.com>
2020-09-17 21:55:41 -0400

    fixing bug where we can't pull from channel

--- slack_client.py
+++ slack_client.py
@@ -58,12 +58,13 @@ def get_standup_replies_for_message(timestamp, channel_name):
             standup_results = []
             for standup_status in replies:
               logger.log("Raw reply is: " + str(standup_status), "INFO")
-              if standup_status.subtype != "bot_message":
+              if standup_status.get("subtype") != "bot_message":
                 # Get username of person who made this reply
                 user_result = SLACK_CLIENT.users_info(user=standup_status['user'])
                 logger.log("User's info is: " + str(user_result), "INFO")
-                logger.log("Adding standup results for " + user_result["user"]["profile"]["real_name"], "INFO")
-                standup_response_for_person = user_result["user"]["profile"]["real_name"] + ": " + standup_status['text'] + "; \n"
+                name = user_result.get("user").get("profile").get("real_name")
+                logger.log("Adding standup results for " + name, "INFO")
+                standup_response_for_person = name + ": " + standup_status.get('text') + "; \n"
                 standup_results.append(standup_response_for_person)
             return standup_results
         else:
